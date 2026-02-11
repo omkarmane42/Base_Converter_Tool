c75c0c726e8d5d7dcb18d6b705206143a83906ff
Omkar Mane <omkarnamdevmane@gmail.com>
2026-02-11 15:14:27 +0530

    Add files via upload

---
+++ Number_System_Calculator.py
@@ -0,0 +1,16 @@
+#Number System Calculator
+
+
+Decimal_Number = int( input("Enter a Decimal Number : "))
+
+#convert into different number system 
+Binary_number = bin(Decimal_Number)
+Octal_number = oct(Decimal_Number)
+Hexadecimal_number = hex(Decimal_Number)
+
+# Show the result
+
+print("\n-------Coversion Result--------")
+print("Binary :",Binary_number)
+print("Ocatal :",Octal_number)
+print("Hexadecimal: ", Hexadecimal_number)
